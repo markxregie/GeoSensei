b958f2dd5e3c0d7bd2d0239b47e766e4477701a6
Mark Regie A. Magtangob <marklegend029@gmail.com>
2025-06-24 15:55:22 +0800

    fixed

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-const chatbotRoutes = require("./chatbot");
+const chatbotRoutes = require("/backend/chatbot.js");
 const { db } = require("./db");  // Import SQLite connection
 require("dotenv").config();
 
